20c300b1657911f2bb9ae360a05b9bfbb2cf4fba
zhupengfei <zhupengfei@wm510.com>
2018-10-05 13:58:43 +0800

    update

--- server/README.md
+++ server/README.md
@@ -6,10 +6,11 @@ MergeGame服务端接口
 
 >参数：userId
 
->返回：data为null，表示已注册
+>返回：errcode:0成功，9001存在多个账号
 
     {
         msgId: 1001,
         errcode: 0,
+        errmsg:'ok'
         data: data    
     }
--- server/db/db.js
+++ server/db/db.js
@@ -33,43 +33,68 @@ module.exports = {
     },
 
     isAccountExist(userId, cb) {
-        User.find({
+        let condition = {
             userId: userId
-        }, (err, res) => {
+        };
+        User.find(condition, function (err, docs) {
+            let flag = null;
             if (err) {
                 console.error('err: ', err);
                 return;
             }
-            if (cb) {
-                res.length > 0 ? cb(true) : cb(false);
+            if (docs.length === 0) {
+                flag = false;
+            } else if (docs.length === 1) {
+                flag = true;
+            } else if (docs.length > 1) {
+                flag = null;
+                return;
             }
-        });
+            if (cb) {
+                cb(flag);
+            }
+        }.bind(this));
     },
 
     createAccount(userId, cb) { //创建账号
-        this.isAccountExist(userId, (bool) => {
-            if (bool) {
-                console.log('====account has already existed====');
+        this.isAccountExist(userId, (flag) => {
+            if (flag === null) {
+                console.log('====同时存在多个账号====');
                 if (cb) {
                     cb(null);
                 }
                 return;
             }
-            let data = {
-                userId: userId,
-                gold: 1000,
-                diamond: 50,
-                medal: 0,
-                historyGold: 1000
-            };
-            User.create(data, (err, docs) => {
-                if (err) {
-                    console.error('err: ', err);
-                    return;
-                }
-                cb(docs);
-                console.log('====create account successful====');
-            });
-        })
+            if (flag === true) {
+                User.find({
+                    userId: userId
+                }, (err, docs) => {
+                    if (err) {
+                        console.error('err: ', err);
+                        return;
+                    }
+                    if (cb) {
+                        cb(docs[0]);
+                    }
+                });
+            } else {
+                let data = {
+                    userId: userId,
+                    gold: 2000,
+                    historyGold: 2000,
+                    medal: 0,
+                    diamond: 0
+                };
+                User.create(data, (err, docs) => {
+                    if (err) {
+                        console.error('err: ', err);
+                        return;
+                    }
+                    if (cb) {
+                        cb(docs);
+                    }
+                });
+            }
+        });
     }
 }
